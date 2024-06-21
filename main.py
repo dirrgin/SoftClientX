6a5b15697ebbdef663a64de231351e7a11ba3ac2
doublebubblemethod <antihobbit@protonmail.com>
2024-06-21 18:25:42 +0200

    lost main

--- main.py
+++ main.py
@@ -3,38 +3,25 @@ import sys
 from AbstractDevice import productionDevice, asyncio, ua
 from asyncua import Client
 from ProductionLineAgent import \
-    IoTHubModuleClient, \
-    d2c, \
-    twin_reported, \
+    d2c, connect_to_devices,\
+    twin_reported, clean_twin,\
     receive_twin_desired, \
     take_direct_method, d2c_Error
-from azure.iot.hub import IoTHubRegistryManager
-from azure.iot.hub.models import Twin, TwinProperties
+from HubDevice import prepareIngredients, send_device_ids, poll_create_connections, os, load_dotenv
 
-async def clear_desired_twin(iothub_registry_manager, device):
-    twin = iothub_registry_manager.get_twin(device)
-    des = twin.properties.desired
-    del des["$metadata"]
-    del des["$version"]
-    for key, value in des.items():
-        des[key] = None
-    twin_patch = Twin(properties=TwinProperties(desired=des))
-    twin = iothub_registry_manager.update_twin(device, twin_patch, twin.etag)
-
+load_dotenv()
+nodes=[] #the list of nodes from the opc ua
+lst_machines=[] #the list of productionDevice instances, use them for the opc ua methods
+lst_dev_err_new = [] #the list of errors 
+unique_names=[] #the list of iot device names
+productionLine=[] #the list of iot hub clients
+connections = [] #the list of iot devices connection strings
 async def main():
-    opcua_endpoint = "opc.tcp://10.103.0.110:4840/"
+    opcua_endpoint = os.getenv('OPC_UA_ENDPOINT')
     #opcua_endpoint = "opc.tcp://172.20.10.10:4840/"
-    CONNECTION_STRING = "HostName=Cirencester-End.azure-devices.net;DeviceId=demo_device1;SharedAccessKey=rq4bvlv6Jd3UEKJL6zHVt2IuwdpKbHhwMAIoTHcgMho="
-    CONNECTION_STRING_MANAGER="HostName=Cirencester-End.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=4THcWqn7NQdn21nYhOcsUm6iWCa3Z4knLAIoTDcDVKI="
-    client_opc = Client(opcua_endpoint)
-
-    DEVICE_ID = "demo_device1"
-    iothub_registry_manager = IoTHubRegistryManager(CONNECTION_STRING_MANAGER)
-    await clear_desired_twin(iothub_registry_manager, DEVICE_ID)
-    
-
     # Connection to OPC UA server
     try:
+        client_opc = Client(opcua_endpoint)
         await client_opc.connect()
     except Exception as e:
         print("OPC UA connection failed")
@@ -42,62 +29,51 @@ async def main():
         sys.exit(1)
     else:
         print("OPC UA connection success")
+    # Connect, create, start
+    nodes = await client_opc.get_objects_node().get_children()
+    nodes = nodes[1:]
+    '''unique_names = await prepareIngredients(nodes)
+    await send_device_ids(unique_names)
+    await asyncio.sleep(4)
+    connections = await poll_create_connections()'''
+    unique_names = ['OPCUA_Device_1_irctm3', 'OPCUA_Device_2_dqfv7p']
+    connections = ['HostName=Cirencester-End.azure-devices.net;DeviceId=OPCUA_Device_1_irctm3;SharedAccessKey=mVMKbqroNNuNKW/25TW1W+cZM7EQhj/3Z3Dyj1nCWeA=', 'HostName=Cirencester-End.azure-devices.net;DeviceId=OPCUA_Device_2_dqfv7p;SharedAccessKey=kbpmc4fWZu89SxxW8TV+d/yt+Nl6Ij4woMXPT8cYDCA=']
+    productionLine = await connect_to_devices(connections)
+    
+    for i in range(len(nodes)):
+        nodeRepr = client_opc.get_node(nodes[i])
+        machine = productionDevice(client_opc, nodeRepr, unique_names[i])
+        lst_machines.append(machine)
+        lst_dev_err_new.append(None)
 
-    # Connection to IoTHub
-    try:
-        client_iot = IoTHubModuleClient.create_from_connection_string(CONNECTION_STRING)
-        client_iot.connect()
-    except Exception as e:
-        print("IoTHub connection failed")
-        print(f"Error: {e}")
-        sys.exit(1)
-    else:
-        print("IoTHub connection success")
+    # Clean reported twins
+    clean_twin(productionLine)
 
-    # Clean the reported twin
-    twin = client_iot.get_twin()['reported']
-    twin.pop("$version", None)
-    for key, value in twin.items():
-        twin[key] = None
-    client_iot.patch_twin_reported_properties(twin)
-    # list of errors
-    lst_dev_err_new = []
     try:
-        while True:
-            # list of actual devices
-            lst_machines = []
-            lst = await client_opc.get_objects_node().get_children()
-            lst = lst[1:]
-            # write devices in list and update data
-            for i in range(len(lst)):
-                nodeRepr = client_opc.get_node(lst[i])
-                device = productionDevice(client_opc, nodeRepr)
-                await device.getDevProp()
-                lst_machines.append(device)
-                if i >= len(lst_dev_err_new):
-                    lst_dev_err_new.append(device.error)
-            await receive_twin_desired(client_iot, lst_machines)
-            
+        while True: 
+            ''' # are there new devices ? update the productionLine : continue 
+            lst = await client_opc.get_objects_node().get_children() 
+            lst = lst[1:] if len(nodes)<len(lst): new_devices = [] [new_devices.append(x) for x in range(len(nodes), len(lst))] uniqueIds = await prepareIngredients(new_devices) await send_device_ids(uniqueIds) await asyncio.sleep(4) connection_strings = await poll_create_connections()
+            new_devices = await connect_to_devices(connection_strings, client_connection_str) [productionLine.append(new_device) for new_device in new_devices]'''
+            # update data
             for j in range(len(lst_machines)):
                 try:
-                    print("@")
-                    await d2c(client_iot, lst_machines[j])
-                    await twin_reported(client_iot, lst_machines[j])
-                    
-                    if lst_dev_err_new[j]!=lst_machines[j].error:
-                        await d2c_Error(client_iot, lst_machines[j])
+                    await lst_machines[j].getDevProp()
+                    if lst_dev_err_new[j] != lst_machines[j].error:
+                        await d2c_Error(productionLine[j], lst_machines[j])
                         lst_dev_err_new[j] = lst_machines[j].error
-                        print("...")
-                        
+                        print("Send D2C - new Error State")
+                    await receive_twin_desired(productionLine[j], lst_machines[j])
+                    await d2c(productionLine[j], lst_machines[j])
+                    await twin_reported(productionLine[j], lst_machines[j])
+                    print("Send D2C , Twin Reported") 
+                    await take_direct_method(productionLine[j], lst_machines[j])      
                 except asyncio.TimeoutError:
                     print("Timeout error while sending d2c/twin_reported to IoT Hub")
                 except asyncio.CancelledError:
                     print("Task was cancelled")
                 except Exception as e:
                     print(f"An unexpected error occurred: {e}")
-
-            await take_direct_method(client_iot, client_opc)
-
             time.sleep(10)
     except KeyboardInterrupt:
         print("Keyboard stopped program")
